8193fb2fb384bf0ce68807d397f07d62547d80b5
Jihye Park <deviloper.park@gmail.com>
2024-05-07 15:04:39 +0900

    Initial Commit

---
+++ JRNL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JRNL
+//
+//  Created by 박지혜 on 5/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
